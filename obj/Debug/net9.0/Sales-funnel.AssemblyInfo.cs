984164e8934d34aa3b34a61acee9f164bf91280f
Matheus De O Zacanini <zacanini2@gmail.com>
2025-04-29 14:46:36 -0300

    feat : termiando aplicaçao front end

--- obj/Debug/net9.0/Sales-funnel.AssemblyInfo.cs
+++ obj/Debug/net9.0/Sales-funnel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sales-funnel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3738b08b59053a30021fff6b2798e4e00670d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56d5a2d5a82ff65c7a496b75195716ac3ea166c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sales-funnel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sales-funnel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
